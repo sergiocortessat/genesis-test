67779b6eecd8bfb8e0fe0490e1316ebb06e200cb
Sergio Cortes <sergiocortessat@gmail.com>
2021-10-26 11:06:32 +0200

    Add remove test file

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,12 @@ import React from 'react'
 import './Styles/App.scss'
 import Convertor from './Components/convertor'
 import NavBar from './Components/NavBar'
-import Test from './Modules/test'
 
 function App () {
   return (
     <div className="App">
       <NavBar />
       <Convertor />
-      <Test />
     </div>
   )
 }
